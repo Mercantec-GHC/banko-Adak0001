437077b99040c57180b10d130bfe05cc27483779
gaest <adak0001@edu.mercantec.dk>
2023-10-25 13:08:59 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,144 +1,59 @@
 ﻿using System;
-using System.Collections.Generic; // Tilføj using-anvisningen for List
-using BingoCardGenerator; // Tilføj denne anvisning for at bruge BingoCardGenerator
+using System.Linq;
 
-namespace Analysis
+class Program
 {
-    class DistributionAnalysis
+    static string[][] bankoPlade = new string[3][]
     {
-        const int Rows = 3;
-        const int Columns = 9;
-        const int NumbersOnCards = 90;
+        new string[] { "20", "30", "43", "55", "70" },
+        new string[] { "4", "14", "33", "44", "67" },
+        new string[] { "29", "49", "68", "78", "87" }
+    };
 
-        enum AnalysisMode
+    static bool CheckFullRow(string[][] plade) => plade.Any(row => row.All(x => x == "X"));
+
+    static bool CheckTwoFullRows(string[][] plade) => plade.Count(row => row.All(x => x == "X")) >= 2;
+
+    static bool CheckFullPlate(string[][] plade) => plade.All(row => row.All(x => x == "X"));
+
+    static void PrintPlade(string[][] plade)
+    {
+        Array.ForEach(plade, row => Console.WriteLine(string.Join(" ", row)));
+    }
+
+    static void Main()
+    {
+        string trukketTal;
+        bool fuldrække1 = false, fuldrække2 = false, fuldPlade = false;
+
+        do
         {
-            Frequency,
-            Percentage
-        }
+            Console.Clear();
+            PrintPlade(bankoPlade);
+            Console.WriteLine("Indtast et tal (eller 'exit' for at afslutte):");
+            trukketTal = Console.ReadLine();
 
-        public double[] EndNumberAnalysis(string key, int amount, AnalysisMode mode)
-        {
-            int endNumbers = 10;
-            int totalNumbers = 0;
-            double[] endNumberFreq = new double[endNumbers];
-            double[] endNumberPct = new double[endNumbers];
-
-            Generator gen = new Generator(key); // Dette er fra BingoCardGenerator
-            List<int[,]> analysedCards = gen.GenerateCard(amount);
-
-            foreach (int[,] item in analysedCards)
+            if (trukketTal == "exit")
             {
-                if (item[0, 0] != -1)
+                break;
+            }
+
+            // Gennemgå bankopladerne og marker det trukne tal med "X"
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
                 {
-                    for (int columnCount = 0; columnCount < Columns; columnCount++)
+                    if (bankoPlade[i][j] == trukketTal)
                     {
-                        for (int rowCount = 0; rowCount < Rows; rowCount++)
-                        {
-                            if (item[columnCount, rowCount] != 0)
-                            {
-                                int endNumber = item[columnCount, rowCount] % 10;
-                                endNumberFreq[endNumber]++;
-                                totalNumbers++;
-                            }
-                        }
+                        bankoPlade[i][j] = "X";
                     }
                 }
             }
 
-            if (mode == AnalysisMode.Frequency)
-                return endNumberFreq;
-            else if (mode == AnalysisMode.Percentage)
-            {
-                for (int i = 0; i < endNumbers; i++)
-                {                    endNumberPct[i] = (endNumberFreq[i] / totalNumbers) * 90;
-                }
-                return endNumberPct;
-            }
-            return null; // Return null for invalid mode
-        }
+            // Resten af koden for at tjekke fulde rækker og plade (som tidligere)
 
-        public double[,] PlacementAnalysis(string key, int amount, AnalysisMode mode)
-        {
-            int totalNumbers = 0;
-            double[,] placeFreq = new double[Columns, Rows];
-            double[,] placePct = new double[Columns, Rows];
+        } while (!(fuldrække1 || fuldrække2 || fuldPlade));
 
-            Generator gen = new Generator(key); // Dette er fra BingoCardGenerator
-            List<int[,]> analysedCards = gen.GenerateCard(amount);
-
-            foreach (int[,] item in analysedCards)
-            {
-                if (item[0, 0] != -1)
-                {
-                    for (int columnCount = 0; columnCount < Columns; columnCount++)
-                    {
-                        for (int rowCount = 0; rowCount < Rows; rowCount++)
-                        {
-                            if (item[columnCount, rowCount] != 0)
-                            {
-                                placeFreq[columnCount, rowCount]++;
-                                totalNumbers++;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (mode == AnalysisMode.Frequency)
-                return placeFreq;
-            else if (mode == AnalysisMode.Percentage)
-            {
-                for (int columnCount = 0; columnCount < Columns; columnCount++)
-                {
-                    for (int rowCount = 0; rowCount < Rows; rowCount++)
-                    {
-                        placePct[columnCount, rowCount] = (placeFreq[columnCount, rowCount] / totalNumbers) * 90;
-                    }
-                }
-                return placePct;
-            }
-            return null; // Return null for invalid mode
-        }
-
-        public double[] NumberAnalysis(string key, int amount, AnalysisMode mode)
-        {
-            int totalNumbers = 0;
-            double[] numberFreq = a double[NumbersOnCards];
-            double[] numberPct = a double[NumbersOnCards];
-
-            Generator gen = a Generator(key); // Dette er fra BingoCardGenerator
-            List<int[,]> analysedCards = gen.GenerateCard(amount);
-
-            foreach (int[,] item in analysedCards)
-            {
-                if (item[0, 0] != -1)
-                {
-                    for (int columnCount = 0; columnCount < Columns; columnCount++)
-                    {
-                        for (int rowCount = 0; rowCount < Rows; rowCount++)
-                        {
-                            if (item[columnCount, rowCount] != 0)
-                            {
-                                int number = item[columnCount, rowCount] - 1;
-                                numberFreq[number]++;
-                                totalNumbers++;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (mode == AnalysisMode.Frequency)
-                return numberFreq;
-            else if (mode == AnalysisMode.Percentage)
-            {
-                for (int i = 0; i < NumbersOnCards; i++)
-                {
-                    numberPct[i] = (numberFreq[i] / totalNumbers) * 90;
-                }
-                return numberPct;
-            }
-            return null; // Return null for invalid mode
-        }
+        Console.WriteLine("Spillet er slut!");
     }
 }
